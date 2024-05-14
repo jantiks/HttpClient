39b3f0a4ceb7332cf4d0f6ea7dea27946fad97d6
jantiks <tigran.arsenyan.2018@gmail.com>
2024-05-14 17:54:36 +0200

    documentation for the Response class

--- HttpClient/HttpClient/Response.cpp
+++ HttpClient/HttpClient/Response.cpp
@@ -7,10 +7,31 @@
 
 #include "Response.hpp"
 
+/**
+ * @brief Constructs a new Response object.
+ *
+ * @param statusCode The status code of the HTTP response.
+ * @param headers The headers of the HTTP response.
+ * @param responseText The body of the HTTP response.
+ */
 Response::Response(const long statusCode, const std::map<std::string, std::string>& headers, const std::string& responseText):statusCode_(statusCode), headers_(headers), responseText_(responseText) {};
 
+/**
+ * @brief Constructs a new Response object.
+ *
+ * @param statusCode The status code of the HTTP response.
+ * @param headers The headers of the HTTP response (rvalue reference).
+ * @param responseText The body of the HTTP response (rvalue reference).
+ */
 Response::Response(long statusCode, std::map<std::string, std::string>&& headers, std::string&& responseText):statusCode_(statusCode), headers_(headers), responseText_(responseText) {};
 
+/**
+ * @brief Outputs the HTTP response to an output stream.
+ *
+ * @param os The output stream.
+ * @param response The Response object to output.
+ * @return std::ostream& The output stream.
+ */
 std::ostream& operator<<(std::ostream& os, const Response& response) {
     os << "Status Code: " << response.statusCode() << "\n";
     os << "Headers: \n";
