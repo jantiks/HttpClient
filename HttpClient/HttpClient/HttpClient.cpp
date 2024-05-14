853bfc017b52f62650591b4d5f8d3728ad8b7d33
tigranWellness <tigran.arsenyan@wellnesscoach.live>
2024-05-14 08:28:15 +0200

    capturing headers and status code

--- HttpClient/HttpClient/HttpClient.cpp
+++ HttpClient/HttpClient/HttpClient.cpp
@@ -22,6 +22,8 @@ std::string& HttpClient::performRequest(const std::string& url, const std::vecto
     CURL* curl;
     CURLcode res;
     std::string readBuffer;
+    std::string responseHeader;
+    long statusCode;
     struct curl_slist* curlHeaders = NULL;
 
     curl = curl_easy_init();
@@ -34,6 +36,8 @@ std::string& HttpClient::performRequest(const std::string& url, const std::vecto
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curlHeaders);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_HEADERDATA, &responseHeader);
 
         if(postData) {
             curl_easy_setopt(curl, CURLOPT_POST, 1L);
@@ -43,17 +47,25 @@ std::string& HttpClient::performRequest(const std::string& url, const std::vecto
         res = curl_easy_perform(curl);
         if(res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        } else {
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &statusCode);
         }
 
         curl_slist_free_all(curlHeaders);
         curl_easy_cleanup(curl);
     }
+    
+    std::cout << "ASD STATUS CODE: " << statusCode << "ASD HEADER RESPONSE: " << responseHeader << std::endl;
 
     return readBuffer;
 }
 
-size_t HttpClient::WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
-{
+//size_t HttpClient::HeaderCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+//    ((std::string*)userp)->append((char*)contents, size * nmemb);
+//    return size * nmemb;
+//}
+
+size_t HttpClient::WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
 }
--- HttpClient/main.cpp
+++ HttpClient/main.cpp
@@ -5,35 +5,12 @@
 //  Created by Tigran on 14.05.24.
 //
 
-#include "iostream"
-#include <curl/curl.h>
+#include <iostream>
+#include "HttpClient.hpp"
 
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
-{
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
-}
-
-int main()
-{
-    CURL* curl;
-    CURLcode res;
-    std::string readBuffer;
-
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-    if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://www.example.com");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        res = curl_easy_perform(curl);
-        if(res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        curl_easy_cleanup(curl);
-    }
-    curl_global_cleanup();
-
-    std::cout << readBuffer << std::endl;
-
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    HttpClient::Request("example.com", {}, HTTPMethod::GET);
+    std::cout << "Hello, World!\n";
     return 0;
 }
